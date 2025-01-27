e3a22f2a91cbaffdba71aa5e00c640e090834be1
sansRebel <khaledalsinafi101@gmail.com>
2025-01-27 22:28:59 +0800

    Added Middleware to save recommendation route

--- src/routes/recRoutes.ts
+++ src/routes/recRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { generateRecommendations, saveRecommendation } from '../controllers/recController';
+import { authenticateUser } from '../middlewares/authMiddlewares';
 
 const router = express.Router();
 
 router.post("/", generateRecommendations);
-router.post("/save", saveRecommendation);
+router.post("/save", authenticateUser, saveRecommendation);
 
 export default router;
