07fe51caf64d59f562b9e53b26312c3ef0f9610b
sansRebel <khaledalsinafi101@gmail.com>
2025-02-02 22:03:49 +0800

    Added cors to dialogflow route

--- src/routes/dialogflowRoutes.ts
+++ src/routes/dialogflowRoutes.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import cors from "cors";
 import { dialogflowWebhook } from "../controllers/dfWebhookController";
 
 const router = express.Router();
+
+router.use(
+    cors({
+        origin: process.env.FRONTEND_URL || "http://localhost:3000",
+        credentials: true,
+    })
+);
 
 // Webhook route for Dialogflow
 router.post("/webhook", dialogflowWebhook);
